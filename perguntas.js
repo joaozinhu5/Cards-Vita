939baff6178e28c6df38d2c1115981fa6025f86b
joaozinhu5 <146849801+joaozinhu5@users.noreply.github.com>
2024-11-11 08:10:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,66 @@
+criaCartao(
+    'Voleibol',
+    'Geralmente quantos jogadores(as) ficam em quadra?',
+    'Geralmente ficam 6 jogadores(as), 7 com o revezamento do líbero'
+)
+
+criaCartao(
+    'Voleibol',
+    'Quando foi criado?',
+    'O vôlei foi criado em 1895, pelo americano William G. Morgan'
+)
+
+criaCartao(
+    'Voleibol',
+    'O que é passe?',
+    'Um fundamento usado para iniciar uma jogada'
+)
+
+criaCartao(
+    'Voleibol',
+    'O que é ataque?',
+    'Fundamento ultilizado para finalizar uma jogada'
+)
+
+criaCartao(
+    'Voleibol',
+    'Quanto mede a altura da rede de voleibol?',
+    'A rede fica a uma altura de 2,43m do solo'
+)
+criaCartao(
+    'Voleibol',
+    'Como se realiza um bloqueio?',
+    'As mãos e braços do jogador que faz o bloqueio pode avançar a rede do adversário, mas apenas com o objetivo de bloquear a passagem da bola.'
+)
+criaCartao(
+    'Voleibol',
+    'Em que país surgiu?',
+    'Nos Estados Unidos.'
+)
+criaCartao(
+    'Voleibol',
+    'Para que o líbero serve?',
+    'Serve para defender e passar simplismente, ele não ataca, não saca e não bloqueia.'
+)
+criaCartao(
+    'Voleibol',
+    'Qual o resultado de um toque na rede?',
+    'Resulta em ponto para o adversário, uma das regras do voleibol é que nao pode tocar na rede de forma alguma, a não ser pela parte de fora.'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
